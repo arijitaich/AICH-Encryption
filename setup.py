1dbad72f05dd69ceb8d33a0dbf2f09694570db5e
Arijit Aich <a2.arijitaich@gmail.com>
2022-04-20 22:15:00 +0530

    Updated Core Algorithm

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aich",
-    version="0.0.2",
+    version="0.0.3",
     author="Arijit Aich",
     author_email="a2.arijitaich@gmail.com",
     description="Encrypt & Decrypt Words Found In A Local Dictionary. Helpful In Tokenizing Blockchain Wallet Passwords.",
