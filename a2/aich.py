c9bede5c61f4906ded1c429b6b0c5c8591792a1e
Arijit Aich <a2.arijitaich@gmail.com>
2022-04-21 21:07:28 +0530

    Removed use same name again for different context.

--- a2/aich.py
+++ a2/aich.py
@@ -21,22 +21,24 @@ def aichin(search_word):
             encrypted = encrypted + str(position) + '.'        
     return (encrypted)
 
+
+
 # Decrypt
 # ================
-def aichout(search_word):
-    phrase = search_word.split('.')
-    words = {}
-    ctr = 0
-    encrypted = ''
+def aichout(search_word2):
+    phrase2 = search_word2.split('.')
+    words2 = {}
+    ctr2 = 0
+    decrypted = ''
     with open('a2/dict.txt') as f:
-            lines = [line.rstrip('\n') for line in f]
-    for line in lines:  
-            words[ctr] = line.lower()
-            ctr = ctr + 1
+            lines2 = [line2.rstrip('\n') for line2 in f2]
+    for line2 in lines2:  
+            words2[ctr2] = line2.lower()
+            ctr2 = ctr2 + 1
 
     # print (phrase)
-    for p in phrase:        
-            if (int(p, 16)):
-                    position = words[int(p, 16)]
-            encrypted = encrypted + str(position) + ' '        
-    return (encrypted)
+    for p2 in phrase2:        
+            if (int(p2, 16)):
+                    position2 = words2[int(p2, 16)]
+            decrypted = decrypted + str(position2) + ' '        
+    return (decrypted)
