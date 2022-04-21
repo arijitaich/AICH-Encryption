8a725122cbdea529d1193cd8b650692c7750b0fd
Arijit Aich <a2.arijitaich@gmail.com>
2022-04-22 00:21:27 +0530

    updated algorithm with file access function

--- a2/aich.py
+++ a2/aich.py
@@ -1,18 +1,35 @@
 
+# File Processor
+def readictionary(handle):
+    filename = 'a2/dict.txt'
+    words = {}
+    ctr = 0
+    with open(filename) as f:
+        lines = [line.rstrip('\n') for line in f]
+    for line in lines:  
+                if(handle == 'enc'):
+                        words[line.lower()] = ctr
+                else:
+                        words[ctr] = line.lower()
+
+                ctr = ctr + 1
+    return words
+
+
 
 # Encrypt
 # ================
 def aichin(search_word):
-    
-    phrase = search_word.split(' ')
     words = {}
-    ctr = 0
+    phrase = search_word.split(' ')
     encrypted = ''
-    with open('a2/dict.txt') as f:
-            lines = [line.rstrip('\n') for line in f]
-    for line in lines:  
-            words[line.lower()] = ctr
-            ctr = ctr + 1
+    words = readictionary()
+#     with open('a2/dict.txt') as f:
+#             lines = [line.rstrip('\n') for line in f]
+#     for line in lines:  
+#             words[line.lower()] = ctr
+#             ctr = ctr + 1
+    
     for p in phrase:
             if p.lower() in words:
                 position = hex(words.get(p.lower()))
@@ -26,20 +43,20 @@ def aichin(search_word):
 # Decrypt
 # ================
 def aichout(search_word2):
-    phrase2 = search_word2.split('.')
-    words2 = {}
-    ctr2 = 0
-    decrypted = ''
-    with open('a2/dict.txt') as f:
-            lines2 = [line2.rstrip('\n') for line2 in f2]
-    for line2 in lines2:  
-            words2[ctr2] = line2.lower()
-            ctr2 = ctr2 + 1
-            
+        phrase2 = search_word2.split('.')
+        words2 = readictionary()
+        ctr2 = 0
+        decrypted = ''
+        # with open('Work/words.txt') as f2:
+        #         lines2 = [line2.rstrip('\n') for line2 in f2]
+        # for line2 in lines2:  
+        #         words2[ctr2] = line2.lower()
+        #         ctr2 = ctr2 + 1
 
-    # print (phrase)
-    for p2 in phrase2:        
-            if (int(p2, 16)):
-                    position2 = words2[int(p2, 16)]
-            decrypted = decrypted + str(position2) + ' '        
-    return (decrypted)
+        for p2 in phrase2:
+                try:
+                        tmp = words2[int(p2, 16)]
+                except:
+                        tmp = p2
+                decrypted = decrypted + str(tmp) + ' '        
+        print (decrypted)
